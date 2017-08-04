5416904dc4d8443777e785fea31e41f3aca824f6
tmbb <tmbb@campus.ul.pt>
2017-08-04 14:51:14 +0100

    First release

--- mix.exs
+++ mix.exs
@@ -5,14 +5,32 @@ defmodule Makedown.Mixfile do
     [
       app: :makedown,
       version: "0.1.0",
-      elixir: "~> 1.5-rc",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       docs: [
         source_url: "https://github.com/tmbb/makedown",
         homepage_url: "https://github.com/tmbb/makedown",
         extras: ["README.md"]
-      ]
+      ],
+      # Package
+      package: package(),
+      description: description()
+    ]
+  end
+
+  defp description do
+    """
+    Markdown implementation that uses Makeup for syntax highlighting.
+    """
+  end
+
+  defp package do
+    [
+      name: :makedown,
+      licenses: ["BSD"],
+      maintainers: ["Tiago Barroso <tmbb@campus.ul.pt>"],
+      links: %{"GitHub" => "https://github.com/tmbb/makedown"}
     ]
   end
 
@@ -29,6 +47,7 @@ defmodule Makedown.Mixfile do
       {:makeup, "~> 0.1.3"},
       {:earmark, "~> 1.1"},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+      #{:ex_doc, github: "elixir-lang/ex_doc", only: :dev, runtime: false}
     ]
   end
 end
